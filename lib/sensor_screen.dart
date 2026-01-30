574147d2711a4648f4c4645c2f723c95ec26a0ab
fschiappacasse <fschiappacasse@uc.cl>
2026-01-30 12:27:15 -0300

    grafico para testing

--- lib/sensor_screen.dart
+++ lib/sensor_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'exercise.dart';
 import 'package:vibration/vibration.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 
 void vibracion() {
@@ -25,10 +26,14 @@ class SensorScreen extends StatefulWidget {
 }
 
 class _SensorScreenState extends State<SensorScreen> {
-  double x = 0, y = 0, z = 0, reps = 0;
+  double x = 0, y = 0, z = 0;
+  int reps = 0;
   bool cooldown = false;
   late StreamSubscription<UserAccelerometerEvent> _subscription;
   DateTime lastRepTime = DateTime.now().subtract(Duration(seconds: 1));
+  List<double> xData = [];
+  List<double> yData = [];
+  List<double> zData = [];
 
 
   @override
@@ -42,6 +47,18 @@ class _SensorScreenState extends State<SensorScreen> {
         x = event.x;
         y = event.y;
         z = event.z;
+
+        xData.add(event.x);
+        yData.add(event.y);
+        zData.add(event.z);
+
+        // Limitar la longitud de la lista para no crecer infinito
+        if (xData.length > 500) {
+          xData.removeAt(0);
+          yData.removeAt(0);
+          zData.removeAt(0);
+        }
+
 
         
 
@@ -82,24 +99,88 @@ class _SensorScreenState extends State<SensorScreen> {
 
 
   @override
-  Widget build(BuildContext context) { // dibuja la interfaz del widget
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'X: ${x.toStringAsFixed(2)}\n'
-          'Y: ${y.toStringAsFixed(2)}\n'
-          'Z: ${z.toStringAsFixed(2)}\n'
-          'reps: ${reps.toStringAsFixed(2)}',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Texto de valores
+            Text(
+              'X: ${x.toStringAsFixed(2)}\n'
+              'Y: ${y.toStringAsFixed(2)}\n'
+              'Z: ${z.toStringAsFixed(2)}\n'
+              'Reps: $reps',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Gráfico con altura limitada
+            Flexible(
+              fit: FlexFit.loose,
+              child: SizedBox(
+                height: 300, // Ajusta la altura como quieras
+                child: LineChart(
+                  LineChartData(
+                    minY: -10,
+                    maxY: 10,
+                    titlesData: FlTitlesData(
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          interval: 2,
+                          getTitlesWidget: (value, meta) {
+                            return Text(
+                              value.toInt().toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      bottomTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      rightTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      topTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    ),
+                    gridData: FlGridData(show: true),
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: List.generate(
+                            xData.length, (i) => FlSpot(i.toDouble(), xData[i])),
+                        isCurved: true,
+                        color: Colors.red,
+                      ),
+                      LineChartBarData(
+                        spots: List.generate(
+                            yData.length, (i) => FlSpot(i.toDouble(), yData[i])),
+                        isCurved: true,
+                        color: Colors.green,
+                      ),
+                      LineChartBarData(
+                        spots: List.generate(
+                            zData.length, (i) => FlSpot(i.toDouble(), zData[i])),
+                        isCurved: true,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-
