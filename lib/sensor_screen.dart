d8fba58dbf0655d712f1da96e3dd993d4a3abe61
fschiappacasse <fschiappacasse@uc.cl>
2026-01-30 10:57:52 -0300

    vibracion

--- lib/sensor_screen.dart
+++ lib/sensor_screen.dart
@@ -2,10 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'exercise.dart';
+import 'package:vibration/vibration.dart';
+
 
 class SensorScreen extends StatefulWidget {
   final Exercise exercise;
-
   SensorScreen({super.key, required this.exercise});
 
   @override
@@ -13,11 +14,11 @@ class SensorScreen extends StatefulWidget {
 }
 
 class _SensorScreenState extends State<SensorScreen> {
-  double x = 0, y = 0, z = 0;
+  double x = 0, y = 0, z = 0, reps = 0;}
   late StreamSubscription<UserAccelerometerEvent> _subscription;
 
   @override
-  void initState() {
+  void initState() { // se llama una vez al inicio
     super.initState();
 
     _subscription = userAccelerometerEvents.listen((event) {
@@ -27,25 +28,52 @@ class _SensorScreenState extends State<SensorScreen> {
         x = event.x;
         y = event.y;
         z = event.z;
+        
+
+        switch (widget.exercise.name) {
+          case 'squat':
+            if( y > 3 && ){
+              reps += 1;
+
+              // Vibrar el teléfono
+              Vibration.hasVibrator().then((hasVibrator) {
+                if (hasVibrator ?? false) {
+                  Vibration.vibrate(duration: 100); // vibra 100 ms
+                }
+              });
+            }
+            break;
+          case 'jump':
+            // Lógica para flexiones
+            break;
+          case 'balance':
+            // Lógica para sentadillas
+            break;
+          default:
+            // Lógica por defecto
+            break;
+        }
       });
     });
   }
 
-  @override
+  @override // cuando se elimina el widget
   void dispose() {
     _subscription.cancel();
     super.dispose();
   }
 
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // dibuja la interfaz del widget
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Text(
           'X: ${x.toStringAsFixed(2)}\n'
           'Y: ${y.toStringAsFixed(2)}\n'
-          'Z: ${z.toStringAsFixed(2)}',
+          'Z: ${z.toStringAsFixed(2)}\n'
+          'reps: ${reps.toStringAsFixed(2)}',
           textAlign: TextAlign.center,
           style: const TextStyle(
             color: Colors.white,
@@ -56,3 +84,5 @@ class _SensorScreenState extends State<SensorScreen> {
     );
   }
 }
+
+
